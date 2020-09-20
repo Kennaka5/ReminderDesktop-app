4dfdf1efeb3febdebf7ddbb79a9029c6af3e25f4
Ken Nakayama <kennaka5@gmail.com>
2020-09-20 16:49:25 -0700

    hooked up ymal reader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 require('dotenv').config()
+const fs = require('fs');
+const yaml = require('js-yaml');
 
+let fileContents = fs.readFileSync('./.reminder.yaml', 'utf8');
+let data = yaml.safeLoad(fileContents);
 
-console.log(process.env.REMINDER_EMAIL)
+console.log(data)
