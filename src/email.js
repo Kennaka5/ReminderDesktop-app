8b077d8ec10afab7ea78e1b5a45a5e292a05c301
Ken Nakayama <kennaka5@gmail.com>
2020-09-22 16:54:09 -0700

    working on timing issue

---
+++ src/email.js
@@ -0,0 +1,18 @@
+
+
+// const mailTransporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.REMINDER_EMAIL,
+//         pass: process.env.REMINDER_EMAIL_PASSWORD,
+//     }
+// });
+
+  
+// mailTransporter.sendMail(dataFile[1], function(err, data) { 
+//     if(err) { 
+//         console.log('Error Occurs', err); 
+//     } else { 
+//         console.log('Email sent successfully'); 
+//     } 
+// }); 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,66 @@
+//dependencies for project
 require('dotenv').config()
 const fs = require('fs');
 const yaml = require('js-yaml');
+const nodemailer = require('nodemailer');
+var CronJob = require('cron').CronJob;
+const notifier = require('node-notifier');
+//bring in Yaml data to work with
+const fileContents = fs.readFileSync('./.reminder.yaml', 'utf8');
+const dataFile = yaml.safeLoad(fileContents);
+const data = new Set(dataFile)
 
-let fileContents = fs.readFileSync('./.reminder.yaml', 'utf8');
-let data = yaml.safeLoad(fileContents);
+const mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+    user: process.env.REMINDER_EMAIL,
+    pass: process.env.REMINDER_EMAIL_PASSWORD,
+    }
+    });
 
-console.log(data)
+//loop through data to get notification types and Schedules
+data.forEach(element => {
+    const types = element.types.split(',')
+    const schedule = element.schedule
+
+/// define function for desktop notifications
+    function desktopNotification() {   
+        notifier.notify({
+            title: 'Desktop Notiication',
+            message: element.message
+        });
+    }
+/// define Email Object
+    const mailInfo = {
+             from: process.env.REMINDER_EMAIL,
+             to: process.env.REMINDER_EMAIL,
+             subject: 'Email Reminder',
+             text: element.message
+         }
+/// define function for email notifications
+    function email() {
+        mailTransporter.sendMail(mailInfo, function(err, data) { 
+            if(err) { 
+                console.log('Error Occurs', err); 
+            } else { 
+                console.log('Email sent successfully'); 
+            } 
+        });
+        }
+//create cron job that uses schedule
+   const job = new CronJob(schedule, function() {
+
+    types.forEach(e => {
+            if(e === 'desktop') {
+            desktopNotification()
+            } else {
+                email()
+            }
+    }, null, true, 'America/Los_Angeles');
+})
+job.start()
+})
+
+
+
+
